7ac11c80bf35da0e4c9761af3270586272401564
SergioAngel-1 <sergioja2003@gmail.com>
2026-01-07 10:03:30 -0500

    Restructure AppLayout component with improved container hierarchy

--- src/shared/layouts/AppLayout.jsx
+++ src/shared/layouts/AppLayout.jsx
@@ -4,12 +4,14 @@ import { ImmersiveBackdrop } from '../organisms/layout/ImmersiveBackdrop'
 import { Footer } from '../organisms/layout/Footer'
 
 export const AppLayout = () => (
-  <div className="relative min-h-screen overflow-x-hidden bg-[#050405] text-white">
+  <div className="relative min-h-screen bg-[#050405] text-white">
     <ImmersiveBackdrop />
-    <Header />
-    <main className="relative mx-auto w-full max-w-full">
-      <Outlet />
-    </main>
-    <Footer />
+    <div className="relative">
+      <Header />
+      <main className="relative mx-auto w-full max-w-full">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   </div>
 )
